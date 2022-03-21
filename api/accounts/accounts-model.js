0386d94d496c6f6fe0333b5c8ad031860e3106bc
Savannah <savmaxwell@gmail.com>
2022-03-21 12:38:25 -0600

    wip

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,13 +1,17 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // SELECT * FROM accounts;
+  return db('accounts');
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  // SELECT * FROM accounts WHERE id = id;
+  return db('accounts').where('id', id).first();
 }
 
 const create = account => {
-  // DO YOUR MAGIC
+  // INSERT INTO accounts (name, budget) VALUES (account.name, account.budget)
 }
 
 const updateById = (id, account) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,43 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    //CODE HERE
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    //CODE HERE
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    //CODE HERE
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    //CODE HERE
+  } catch (err) {
+    next(err)
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    //CODE HERE
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
