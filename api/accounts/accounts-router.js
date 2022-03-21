8cce017361b8e4c6c580d3a6e98373848e6a4ae5
Savannah <savmaxwell@gmail.com>
2022-03-21 12:11:07 -0600

    started project

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -21,7 +21,9 @@ router.delete('/:id', (req, res, next) => {
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
