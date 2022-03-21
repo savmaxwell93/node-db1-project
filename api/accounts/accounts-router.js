70bfb693b5af7e5077e0f5435dc978f5ff8dded1
Savannah <savmaxwell@gmail.com>
2022-03-21 13:23:50 -0600

    wipo

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -37,7 +37,8 @@ md.checkAccountPayload,
 md.checkAccountNameUnique,
 async (req, res, next) => {
   try {
-    //CODE HERE
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.status(200).json(updated);
   } catch (err) {
     next(err)
   }
